d1e04737a9baee54ee702b9b592559189a780728
Fermin2107 <fermineyherabide@gmail.com>
2026-01-25 10:50:33 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,14 @@ from extensions import db
 from routes.prestadores import prestadores_bp
 from routes.servicios import servicios_bp
 from routes.valoraciones import valoraciones_bp
+from routes.eventos import eventos_bp
+from routes.metricas import metricas_bp
 
-import models.usuario  # noqa: F401
-import models.prestador  # noqa: F401
-import models.servicio   # noqa: F401
+import models.usuario    # noqa: F401
+import models.prestador # noqa: F401
+import models.servicio  # noqa: F401
 import models.valoracion # noqa: F401
+import models.evento    # noqa: F401
 
 def create_app():
     app = Flask(__name__)
@@ -19,6 +22,8 @@ def create_app():
     app.register_blueprint(prestadores_bp, url_prefix="/prestadores")
     app.register_blueprint(servicios_bp, url_prefix="/servicios")
     app.register_blueprint(valoraciones_bp, url_prefix="/valoraciones")
+    app.register_blueprint(eventos_bp, url_prefix="/eventos")
+    app.register_blueprint(metricas_bp) # sin prefijo por REST
 
     @app.route("/ping")
     def ping():
